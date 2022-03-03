4efd26a50ea8648e35456d770256c225b9a8997e
hoppo97 <>
2022-03-03 23:01:08 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper clear">
+        <header className="d-flex justify-between align-center p-40">
+          <div className="d-flex align-center">
+            <img width={40} height={40} src="/img/logo.png" />
+            <div>
+              <h3 className="text-uppercase">React Sneakers</h3>
+              <p>Магазин лучших кросовок</p>
+            </div>
+          </div>
+          <ul className="d-flex">
+            <li className="mr-30">
+              <img width={18} height={18} src="/img/cart.svg"/>
+              <span>1205 руб.</span>
+            </li>
+            <li>
+              <img width={18} height={18} src="/img/user.svg"/>
+            </li>
+          </ul>
+        </header>
+        <div className="content p-40">
+          <h1>Все кроссовки</h1>
+          ...
+        </div>
     </div>
   );
 }
