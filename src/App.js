01bbee769cbcd64f1f6631cd62138cc393bcb128
hoppo97 <>
2022-03-07 13:47:19 +0300

    Не много доработал Card

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
+const arr = [
+  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.jpg'},
+  {name: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl: '/img/sneakers/2.jpg'},
+  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/sneakers/3.jpg'},
+  {name: 'Кроссовки Puma X Aka Boku Future Rider', price: 3499, imageUrl: '/img/sneakers/4.jpg'},
+];
+
 function App() {
   return (
     <div className="wrapper clear">
         <Drawer />
-        <Header/>
+        <Header />
         <div className="content p-40">
-          <div className="d-flex align-center justify-between mb-40">
+          <div className="d-flex align-center justify-between mb-40"> 
             <h1>Все кроссовки</h1>
             <div className="search-block d-flex">
               <img src="/img/search.svg" alt="Search" />
@@ -17,14 +24,17 @@ function App() {
           </div>
 
           <div className="d-flex">
-            <Card />
-            <Card />
-            <Card />
-            <Card /> 
+            {arr.map((obj) => (
+              <Card  
+                title={obj.name}
+                price={obj.price}
+                imageUrl={obj.imageUrl}
+              />
+            ))}
+            
           </div>
         </div>
     </div>
   );
-}
-
+};
 export default App;
